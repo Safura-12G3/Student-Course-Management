dd3c0c7c4f8be153364627964672df666f98c90c
Safura-12G3 <safurasaif9g3@gmail.com>
2026-01-26 14:30:52 +0400

    Added a login dashboard and student buttons

--- Main.java
+++ Main.java
@@ -18,11 +18,17 @@ import java.util.HashMap;
 public class Main extends Application {
 
     private HashMap<String, String> users = new HashMap<>();
+    private HashMap<String, Student> students = new HashMap<>();
+
     private final String FILE_NAME = "users.json";
+    private final String STUDENTS_FILE = "students.json";
+
     private Gson gson = new Gson();
 
     @Override
     public void start(Stage stage) {
+        private HashMap<String, Student> students = new HashMap<>();
+        private final String STUDENTS_FILE = "students.json";
         loadUsers();
 
         javafx.scene.control.Label userLabel = new javafx.scene.control.Label("Username:");
@@ -55,6 +61,49 @@ public class Main extends Application {
             Label welcomeLabel = new Label("Welcome, " + username + "!");
             Button addStudentButton = new Button("Add Student");
             Button viewStudentsButton = new Button("View Students");
+            
+            Stage addStage = new Stage();
+            addStage.setTtitle("Add Student");
+
+            addStudentButton.setOnAction(ev -> {
+                addStage.setTitle("Add Student:");
+
+                GridPane addGrid = new GridPane();
+                addGrid.setVgap(10);
+                addGrid.setHgap(10);
+                addGrid.setPadding(new javafx.geometry.Insets(20));
+
+                TextField nameField = new TextField();
+                TextField idField = new TextField();
+                TextField courseField = new TextField();
+
+                Button saveButton = new Button("Save");
+
+                addGrid.add( new Label("Name:"), 0, 0);
+                addGrid.add(nameField, 1, 0);
+                addGrid.add(new Label("ID:"), 0, 1);
+                addGrid.add(idField, 1, 1);
+                addGrid.add(new Label("Course:"), 0, 2);
+                addGrid.add(courseField, 1, 2);
+                addGrid.add(saveButton, 1, 3);
+
+                saveButton.setOnAction(e2 -> {
+                    String name = nameField.getText();
+                    String id = idField.getText();
+                    String course = courseField.getText();
+
+                    if (!name.isEmpty() && !id.isEmpty() && !course.isEmpty()) {
+                        Student newStudent = new Student(name, id, course);
+                        students.put(id, newStudent);
+                        addStage.close();
+                        System.out.println("Student added " + name + ", " + id + ", " + course);
+                    }
+                });
+
+                Scene addScene = new Scene(addGrid, 300, 200);
+                addStage.setScene(addScene);
+                addStage.show();
+            });
             
             dashboardLayout.getChildren().addAll(welcomeLabel, addStudentButton, viewStudentsButton);
 
